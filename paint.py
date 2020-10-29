637a628f52f959cf50c7892b9fc0940ef13c9b13
alt9193 <brackets_corchetes@hotmail.com>
2020-10-29 13:35:19 -0600

    Se añadio la funcion del rectangulo

--- paint.py
+++ paint.py
@@ -30,7 +30,7 @@ def square(start, end):
     begin_fill() 
 
     for count in range(4): #do four lines
-        forward(end.x - start.x) 
+        forward(end.x - start.x)
 	#move the turtle the size betwen the first and the second point
         left(90) #change the direction of the turtle
 
@@ -46,7 +46,17 @@ def circle(start, end):
 def rectangle(start, end):
 
     "Draw rectangle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range (2):
+        forward(end.x - start.x)
+        left(90)
+        forward(end.y - start.y)
+        left(90)
+    end_fill()
 
 def triangle(start, end):
     "Draw triangle from start to end."
